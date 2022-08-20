7dba3638b1656bb485e77f5076401780b7b1d333
Luiz Felipe <felipeborgeseng@outlook.com.br>
2022-08-20 14:24:28 -0300

    primeiro commit

---
+++ src/main/java/br/com/pocpulsar/pulsar/PulsarApplication.java
@@ -0,0 +1,13 @@
+package br.com.pocpulsar.pulsar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulsarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/pocpulsar/pulsar/PulsarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pocpulsar.pulsar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
